de390db579d6e2669f830bca87f475ce0485fbad
jolee <jolee@redhat.com>
2017-07-17 16:04:38 -0500

    cvanball -> johnathonlee

--- jdv-ocp-setup.sh
+++ jdv-ocp-setup.sh
@@ -4,22 +4,22 @@ oc login https://10.1.2.2:8443 -u admin -p admin
 echo 'Switching to the openshift project'
 oc project openshift
 echo 'Creating the image stream for the OpenShift datavirt image'
-oc create -f https://raw.githubusercontent.com/cvanball/jdv-ose-demo/master/extensions/is.json
+oc create -f https://raw.githubusercontent.com/johnathonlee/jdv-ose-demo/master/extensions/is.json
 echo 'Creating the s2i quickstart template. This will live in the openshift namespace and be available to all projects'
 oc create -n openshift -f https://raw.githubusercontent.com/jboss-openshift/application-templates/master/datavirt/datavirt63-extensions-support-s2i.json
 oc login -u openshift-dev -p devel
 echo 'Creating a new project called jdv-demo'
 oc new-project jdv-demo
 echo 'Creating a service account and accompanying secret for use by the data virt application'
-oc create -f https://raw.githubusercontent.com/cvanball/jdv-ose-demo/master/extensions/datavirt-app-secret.yaml
+oc create -f https://raw.githubusercontent.com/johnathonlee/jdv-ose-demo/master/extensions/datavirt-app-secret.yaml
 echo 'Add the role view to the service account under which the pod is running'
 oadm policy add-role-to-user view system:serviceaccount:jdv-demo:datavirt-service-account
 echo 'Retrieving datasource properties (market data flat file and country list web service hosted on public internet)'
-curl https://raw.githubusercontent.com/cvanball/jdv-ose-demo/master/extensions/datasources.properties -o datasources.properties
+curl https://raw.githubusercontent.com/johnathonlee/jdv-ose-demo/master/extensions/datasources.properties -o datasources.properties
 echo 'Creating a secret around the datasource properties'
 oc secrets new datavirt-app-config datasources.properties
 echo 'Deploying JDV quickstart template with default values'
-oc new-app datavirt63-extensions-support-s2i -p SOURCE_REPOSITORY_URL=https://github.com/cvanball/jdv-ose-demo,CONTEXT_DIR=vdb,EXTENSIONS_REPOSITORY_URL=https://github.com/cvanball/jdv-ose-demo,EXTENSIONS_DIR=extensions,TEIID_USERNAME=teiidUser,TEIID_PASSWORD=redhat1!
+oc new-app datavirt63-extensions-support-s2i -p SOURCE_REPOSITORY_URL=https://github.com/johnathonlee/jdv-ose-demo,CONTEXT_DIR=vdb,EXTENSIONS_REPOSITORY_URL=https://github.com/johnathonlee/jdv-ose-demo,EXTENSIONS_DIR=extensions,TEIID_USERNAME=teiidUser,TEIID_PASSWORD=redhat1!
 echo '==============================================='
 echo 'The following urls will allow you to access the vdbs (of which there are two) via OData2 and OData4:'
 echo '==============================================='
